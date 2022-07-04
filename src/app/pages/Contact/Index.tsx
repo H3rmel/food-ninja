737c54550bf2fa1c2b74319c970c933d5d3c9206
Isaac Hermel <isaachermel@gmail.com>
2022-07-04 00:51:43 -0300

    style fix: mudei o title de um dos layouts

--- src/app/pages/Contact/Index.tsx
+++ src/app/pages/Contact/Index.tsx
@@ -3,7 +3,7 @@ import Layout from "../../components/Layout/Index";
 
 const Contact = () => {
   return (
-    <Layout title="About">
+    <Layout title="Contact">
       <>
         <div className="w-full h-screen flex justify-center items-center">
           <h1 className="text-3xl">Nothing too see here by now!</h1>
