0fb93339f6c14b98ef453c79f3e5a29595ed09b7
Isaac Hermel <isaachermel@gmail.com>
2022-07-12 21:03:56 -0300

    style: @apply directive more used
    using @apply directive to custom classes on account btns

--- src/app/components/Account/Index.tsx
+++ src/app/components/Account/Index.tsx
@@ -7,12 +7,12 @@ const Account = () => {
   return (
     <div className="flex justify-center md:justify-end gap-2">
       <Tippy content={"This doesn't work!"} placement="bottom" arrow={false} animation="shift-away" theme="translucent">
-        <button className="btn text-primary border-primary md:border-2 hover:bg-primary hover:text-white transition ease-out duration-300">
+        <button className="btn btn-account">
           Log In
         </button>
       </Tippy>
       <Tippy content={"It's just visuals!"} placement="bottom" arrow={false} animation="shift-away" theme="translucent">
-        <button className="btn text-primary border-primary md:border-2 hover:bg-primary hover:text-white transition ease-out duration-300">
+        <button className="btn btn-account">
           Log Out
         </button>
       </Tippy>
